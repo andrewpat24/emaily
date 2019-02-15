0a9a4699facc24c0cdcbb6fa9220cceeb22b5986
Andrew Patterson <apat24@mail.sfsu.edu>
2019-02-14 17:40:59 -0800

    implements basic oauth from google. Can login and cookies are assigned

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+require('dotenv').load();
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express(); 
 
 app.get('/', (req, res) => {
     res.send({
-        hi: 'there'
-    });
-});
+        hello: "world"
+    })
+})
+
+passport.use( new GoogleStrategy({
+    clientID: process.env.GOOGLE_OAUTH_ID, 
+    clientSecret: process.env.GOOGLE_OAUTH_SECRET, 
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+    console.log("access token:", accessToken);
+}) );
+
+app.get('/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google') )
 
 const PORT = process.env.PORT || 5000; 
-
 app.listen( PORT );
