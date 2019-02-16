35311d18d3fa066719b484fb555e31eb69b35f0e
Andrew Patterson <apat24@mail.sfsu.edu>
2019-02-15 17:13:37 -0800

    rearranged index.js to resolve order of operations error with calling mongoDB users collection. Implemented saving user id upon registration

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 require('dotenv').load();
+
+// Express
 const express = require('express');
-const mongoose = require('mongoose');
-
-
-
 const app = express(); 
-require('./services/passport');
 
 // Mongoose
+const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI);
+require('./models/Users');
 
 // Routes 
 const authRouter = require('./routes/auth');
@@ -16,5 +15,9 @@ const rootRouter = require('./routes/root');
 app.use('/auth', authRouter);
 app.use('/', rootRouter);
 
+// Passport
+require('./services/passport');
+
+// PORT
 const PORT = process.env.PORT || 5000; 
 app.listen( PORT );
--- services/passport.js
+++ services/passport.js
@@ -1,12 +1,16 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users'); 
 
 passport.use( new GoogleStrategy({
     clientID: process.env.GOOGLE_OAUTH_ID, 
     clientSecret: process.env.GOOGLE_OAUTH_SECRET, 
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-    console.log("access token:", accessToken);
-    console.log("refresh Token:", refreshToken); 
-    console.log("profile:", profile);
+    console.log('User successfully fetched from google OAuth!');
+    new User({
+        googleId: profile.id
+    }).save();
 }) );
