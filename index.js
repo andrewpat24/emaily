9835b8bad5fba482887afa13d0300167f310d364
Andrew Patterson <apat24@mail.sfsu.edu>
2019-02-15 16:41:01 -0800

    adds models directory. Updates index.js to include remote mongodb connection with mongoose. Renames index route to 'root'

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 require('dotenv').load();
 const express = require('express');
-require('./services/passport');
+const mongoose = require('mongoose');
+
+
 
 const app = express(); 
-const authRouter = require('./routes/auth');
-const indexRouter = require('./routes/index');
+require('./services/passport');
 
+// Mongoose
+mongoose.connect(process.env.MONGO_URI);
+
+// Routes 
+const authRouter = require('./routes/auth');
+const rootRouter = require('./routes/root');
 app.use('/auth', authRouter);
-app.use('/', indexRouter);
+app.use('/', rootRouter);
 
 const PORT = process.env.PORT || 5000; 
 app.listen( PORT );
--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-
-
 router.get('/google', 
     passport.authenticate('google', {
         scope: ['profile', 'email']
