52a8953fe0ecd3cadf8d0545299c1c305a915353
Andrew Patterson <apat24@mail.sfsu.edu>
2019-03-06 17:25:51 -0800

    adds proxy to client.

--- client/src/App.js
+++ client/src/App.js
@@ -9,16 +9,16 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome <code>to</code> Emaily.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
+            href="/auth/google"
             rel="noopener noreferrer"
           >
-            Learn React
+            Login with Google
           </a>
+          
         </header>
       </div>
     );
