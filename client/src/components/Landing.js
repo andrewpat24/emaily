1c5d6eed36e5ea2085b5cf6538825c0dfa26b0f0
Andrew Patterson <apat24@mail.sfsu.edu>
2019-03-14 17:34:29 -0700

    adds react .env files and sample

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const Landing = () => {
+    return (
+        <div style={{
+            textAlign: 'center'
+        }}>
+            <h1>Emaily</h1>
+            <span>Collect feedback from your users</span>
+        </div>
+    )
+}
+
+export default Landing; 
