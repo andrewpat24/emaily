b75606fda5c83595c6ced4202308a7d36dfedc6c
Andrew Patterson <apat24@mail.sfsu.edu>
2019-03-19 15:21:10 -0700

    updates package.json with heroku-postbuild script.

--- index.js
+++ index.js
@@ -41,6 +41,17 @@ app.use('/auth', authRouter);
 app.use('/billing', billingRouter);
 app.use('/', rootRouter);
 
+// Production Client Assets
+if ( process.env.NODE_ENV === 'production' ) {
+    app.use( express.static('client/build') );
+
+    app.get("*", (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        
+    });
+
+}
 
 
 
