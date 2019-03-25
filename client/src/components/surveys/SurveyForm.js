18dcca3f1b450998d64b9d314dbc970571318dbf
Andrew Patterson <apat24@mail.sfsu.edu>
2019-03-25 14:41:36 -0700

    adds surveyform components

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SurveyForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Survey Form</h1>
+            </div>
+        )
+    }
+}
+
+export default SurveyForm; 
