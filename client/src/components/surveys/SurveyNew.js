2b1385a06ff718ce2f5a47efce0423a548cedf66
Andrew Patterson <apat24@mail.sfsu.edu>
2019-03-26 17:21:37 -0700

    adds reduxForm helper to surveyNew so values get cleared when user cancels submission.

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
+import { reduxForm } from 'redux-form';
 
 class SurveyNew extends Component {
     
@@ -39,4 +40,6 @@ class SurveyNew extends Component {
     }
 }
 
-export default SurveyNew; 
+export default reduxForm({
+    form:'surveyForm'
+})(SurveyNew); 
